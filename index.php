3e14e1fd2dae338a0e56fdc2e060662ace85c5a6
gabriel <gabrieldossantosvargas@gmail.com>
2020-05-19 19:32:08 -0300

    tela inicial

--- index.php
+++ index.php
@@ -1,1 +1,63 @@
-<h1>teste</h1>
+<?php require_once "header.php"; ?>
+<section class="header-site">
+    <div class="container ">
+        <div class="row">
+            <div class="col-xs-12">
+                <h1 id="titulo">Mama's House</h1>
+                <p class="lead text-center" id="subTitulo">Pizzas da casa da mamãe</p>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section-white">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-4" style="margin: 10px 0">
+                <p style="font-size: 18px;line-height: 33px">Comer em familia é muito melhor</p>
+                <p id="bemvindo">Bem-vindo</p>
+                <p style="font-family: 'Montserrat', sans-serif; font-size: 35px; font-weight: inherit;line-height: 45px">
+                    Reconhecido pela tradição de mais de 50 anos.</p>
+                <p style="font-size: 16px;line-height: 27px">Premiada pelo instituto Curso em Vídeo de qualidade de
+                    culinária e oferecendo os melhores serviços. Premiada pelo instituto Curso em Vídeo de qualidade de
+                    culinária e oferecendo os melhores serviços</p>
+                <div class="btn btn-black-orange">
+                    Mais sobre nós <i class="fas fa-arrow-right"></i>
+                </div>
+            </div>
+            <div class="col-4" id="img-familia-pizza"><img src="img/mao-pizza.png" class="img-fluid"></div>
+            <div class="col-4 text-center" id="img-pizza-escuro" style="margin: 10px 0">
+                <div style="position:absolute; bottom: 0;right: 0;left: 0;">
+                    <p id="horario">Horário</p>
+                    <p>Todos os dias, de 9h até 23h</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!--<img src="img/food-1932466_1920.jpg" class="img-fluid">-->
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+</body>
