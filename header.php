cfaceddd1260872310cbfbcf4762af34206a820d
gabriel <gabrieldossantosvargas@gmail.com>
2020-05-19 17:08:40 -0300

    First

---
+++ header.php
@@ -0,0 +1,64 @@
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+<!-- Favicon -->
+<!--<link rel="shortcut icon" href="--><?//= $this->asset('img/favicon.ico') ?><!--" type="image/x-icon">-->
+<!--<link rel="icon" href="--><?//= $this->asset('img/favicon.ico') ?><!--" type="image/x-icon">-->
+
+<!-- jQuery -->
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
+
+<!-- Bootstrap -->
+<link
+    rel="stylesheet"
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+    crossorigin="anonymous">
+<script
+    src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+    crossorigin="anonymous"></script>
+
+<!-- FontAwesome -->
+<link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="<?= $this->asset('lib/fontawesome/css/all.css') ?>"/>
+
+<!-- Select 2 -->
+<link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet"/>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
+<link
+    href="<?= $this->asset('lib/select2-bootstrap4-theme/css/select2-bootstrap4.min.css') ?>"
+    rel="stylesheet"/>
+
+<!-- DataPicker-->
+<link rel="stylesheet" type="text/css" href="<?= $this->asset('lib/datapicker/css/bootstrap-datepicker.css') ?>">
+<script src="<?= $this->asset('lib/datapicker/js/bootstrap-datepicker.min.js') ?>"></script>
+<script src="<?= $this->asset('lib/datapicker/js/bootstrap-datepicker.pt-BR.min.js') ?>" charset="UTF-8"></script>
+
+<!-- MaskMoney-->
+<script src="<?= $this->asset('lib/maskmoney/js/maskMoney.min.js') ?>" charset="UTF-8"></script>
+
+<!-- CSS próprio -->
+<link rel="stylesheet" type="text/css" href="<?= $this->asset('css/navbar.css') ?>"/>
+<link rel="stylesheet" type="text/css" href="<?= $this->asset('css/menu_lateral.css') ?>"/>
+<link rel="stylesheet" type="text/css" href="<?= $this->asset('css/main.css') ?>"/>
+
+<!-- JS próprio-->
+<script src="<?= $this->asset('js/functions-custons.js') ?>"></script>
+
+<!--JQUERY VALIDATE-->
+<script src="<?= $this->asset('lib/plugin/jquery.validate.min.js') ?>"></script>
+
+
+<!--JQUERY JQ-FROM-->
+<script src="<?= $this->asset('lib/plugin/jquery-form.min.js') ?>"></script>
+
+<!--DataTables-->
+<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
+<script src="//cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+
+<title>
+    Restaurant
+</title>
+</head>
